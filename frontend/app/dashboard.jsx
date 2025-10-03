4f52440380d537c583f26d435ade002d69e17500
Vutlhari23 <vutlhari23maswanganyi@gmail.com>
2025-10-03 07:47:35 +0200

    I added a map to dashboard

--- frontend/app/dashboard.jsx
+++ frontend/app/dashboard.jsx
@@ -1,14 +1,52 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
+
+const position = [-23.8494, 29.4480];
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 const Dashboard = () => {
   return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
+   
+     <MapContainer
+      center={position}
+      zoom={12}
+      style={{ height: "400px", width: "100%" }}
+    >
+ 
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+
+      <Marker position={position}>
+        <Popup>Hello! This is Johannesburg.</Popup>
+      </Marker>
+    </MapContainer>
+      
+        
+
   )
 }
-
 export default Dashboard
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  box: {
+    width: 400,
+    height: 400,
+    backgroundColor: 'blue',
+    
+    
+  }
+})
